4000280e21348b28678c9c6cf61496c8201bc5bc
bagusnusantara <bagus.nusantara313@gmail.com>
2019-12-20 17:46:32 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', 'DonationController@index')->name('welcome');
+Route::post('/finish', function(){
+    return redirect()->route('welcome');
+})->name('donation.finish');
+
+Route::post('/donation/store', 'DonationController@submitDonation')->name('donation.store');
+Route::post('/notification/handler', 'DonationController@notificationHandler')->name('notification.handler');
+
+
